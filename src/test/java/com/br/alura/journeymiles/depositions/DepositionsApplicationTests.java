4e558ae8bf98f07c8e2bce437c6a7322a2485b1a
Rafael Sugahara Francisco <rafa.s.francisco@gmail.com>
2023-07-14 00:26:25 -0300

    primeiro commit

---
+++ src/test/java/com/br/alura/journeymiles/depositions/DepositionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.alura.journeymiles.depositions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
